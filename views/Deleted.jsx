9104ea6951dff877d2ea83b02ef9278a51d6eb9b
cwstone101 <102690357+cwstone101@users.noreply.github.com>
2022-06-01 20:57:59 -0400

    Add files via upload

--- views/Deleted.jsx
+++ views/Deleted.jsx
@@ -1,5 +1,5 @@
 const React = require('react');
-
+const myBackground = {backgroundImage: "url('https://media.istockphoto.com/photos/delete-key-picture-id157287738?b=1&k=20&m=157287738&s=170667a&w=0&h=4g3OX1x18PJWDfb9d7UY0qOCVcO0R1p4MYuIJbP3bGE=')"}
 const myBody = {
     border: "double 6px black",
     padding: "15px",
@@ -12,8 +12,9 @@ class Deleted extends React.Component{
 
         return(
             <>
-            
-            <nav><br />
+            <body style={myBackground}>
+                
+                        <nav><br />
                 <a href="/home"> Home </a>
                 <br />
             </nav>
@@ -21,8 +22,9 @@ class Deleted extends React.Component{
             <div style={myBody}>
                 Deleted
             </div>
-            
+            </body>
             </>
+
         )
     }
 }
--- views/Edit.jsx
+++ views/Edit.jsx
@@ -1,13 +1,16 @@
 const React = require('react');
-
+const myBackground = {backgroundImage: "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxshMKMoViI_U7ctFO3y-qLiuNqeIqyB2Ieg&usqp=CAU')"
+,backgroundSize: "cover"
+}
 class Edit extends React.Component{
 
     render() {
 
         return(
             <>
-            
-            <nav>,<br />
+            <body style={myBackground}>
+                
+                        <nav>,<br />
                 <a href="/home"> Home </a>
             </nav>
             <form action="/home" method= 'POST'>
@@ -19,7 +22,7 @@ class Edit extends React.Component{
 
             
             </form>
-            
+            </body>
             </>
         )
     }
